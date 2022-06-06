89e0c11653021f3472412e3a1110181f2b7283d3
pavaneshwar <pavaneshwar23@gmail.com>
2022-06-06 20:04:34 +0530

    commit2

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const reviews = [
       name: "anna johnson",
       work: "web designer",
       photo:
-        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+        "person-2.jpg",
       rev:
         "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
     },
@@ -22,7 +22,7 @@ const reviews = [
       name: "peter jones",
       work: "intern",
       photo:
-        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+        "person-3.jpg",
       rev:
         "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
     },
@@ -31,18 +31,61 @@ const reviews = [
       name: "bill anderson",
       work: "the boss",
       photo:
-        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+        "person-4.jpg",
       rev:
         "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
     },
   ];
 
-  let current = 0;
+  //items
+  let photo= document.querySelector('#photo');
+  let people= document.querySelector('#name');
+  let work= document.querySelector('#work');
+  let rev= document.querySelector('#rev');
+  
+  //buttons
+  let leftButton= document.querySelector("#left");
+  let rightButton= document.querySelector("#right");
+  let surpriseButton= document.querySelector("#surprise");
 
-  window.addEventListener('DOMContentLoader',()=>{
+
+
+  let currentItem = 0;
+
+  function showReview(){
+    const item = reviews[currentItem];
+    
+    photo.src=item.photo;
+    people.textConent=item.name;
+    work.textContent=item.work;
+    rev.textContent=item.rev;
+  }
+
+  window.addEventListener('DOMContentLoaded',function () {
+      showReview();
+    });
+
+  
+
+  leftButton.addEventListener("click",()=>{
+    currentItem--;
+    if(currentItem<0){
+      currentItem=reviews.length-1;
+    }
     showReview();
   });
 
-  showReview(){
-    if {}
-  }
+  rightButton.addEventListener("click",()=>{
+    currentItem++;
+    if(currentItem>reviews.length-1){
+      currentItem=0;
+    }
+    showReview();
+  });
+
+  surpriseButton.addEventListener("click",()=>{
+    //currentItem=3;
+    currentItem = Math.floor(Math.random()*(reviews.length-1));
+    showReview();
+  });
+  console.log(currentItem);
